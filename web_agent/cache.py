573be36f6e54e6a5ff088889c2ae0406031e5b5f
Max Roxburgh <max.roxburgh@trilodocs.com>
2025-07-05 15:14:08 +0100

    Fiexed cache module.py bug

--- web_agent/cache.py
+++ web_agent/cache.py
@@ -1,7 +1,7 @@
 import diskcache as dc
 import hashlib
 
-cache = dc.Cache('./gemini_cache')
+Cache = dc.Cache('./gemini_cache')
 
 def get_cache_key(prompt, params=None):
     key_data = prompt + str(params)
--- web_agent/model.py
+++ web_agent/model.py
@@ -1,50 +1,35 @@
 import google.generativeai as genai
 import cache
-from config import GEMINI_API_KEY
-
-
-genai.configure(api_key=GEMINI_API_KEY)
-
-
-'''
-Pick the model to use, currently set to gemini-2.5-flash
-
-Options include: 
-    - gemini-2.5-flash; 15 RPM, 1500 RPD, 1mil TPM 
-    - gemini-2.5-pro; 60 RPM, 1000 RPD, ~6mil token cap/day
-    - gemini-2.5-flash-lite-preview-06-17; 500 RPD
-    - gemini-2.0-flash-lite-001; 
-    - gemini-2.0-flash-preview-image-generation
-    - gemini-embedding-exp-03-07
-'''
-
-
-model = genai.GenerativeModel("gemini-2.5-flash")
 
 
 def gemini_api_call(
         prompt: str,
+        model_name: str = "gemini-2.5-flash",
         context: str | None = None
 ) -> str:
 
+    from config import GEMINI_API_KEY
+    genai.configure(api_key=GEMINI_API_KEY)
+    model = genai.GenerativeModel(model_name)
     response = model.generate_content(prompt if not context else [context, prompt])
     return response.text
+
 
 def generate(
         prompt: str,
         context: str | None = None
 ) -> str:
 
-    key = cache.get_cache_key(prompt, context)
+    key = cache.Cache.get_cache_key(prompt, context)
 
-    if key in cache:
+    if key in cache.Cache:
         print("Cache hit")
-        return cache[key]
+        return cache.Cache[key]
 
     print("Calling Gemini API")
     response = gemini_api_call(prompt, context)
-    cache[key] = response
-    return response.text
+    cache.Cache[key] = response
+    return response
 
 # def generate_with_mcp(prompt):
 #     response = vertex_agent.invoke(prompt)
