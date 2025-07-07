6b08d467cfa6cf975872c2f3f19d0bb90a2c0691
Max Roxburgh <max.roxburgh@trilodocs.com>
2025-07-08 00:10:44 +0100

    removed config

--- web_agent/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-GEMINI_API_KEY = "AIzaSyBuO69b3_zlJ4LBcQFMiE-x2fJRBnrstjg"
-SEARCH_API_KEY = "AIzaSyC-b4rI8u9Tb-8DJPxoYF2odttI8Amm4kM"
-# GEMINI_API_KEY: str | None = os.getenv("GEMINI_API_KEY")
-# SEARCH_API_KEY: str | None = os.getenv("SEARCH_API_KEY")
-SEARCH_ENGINE_ID: str | None = "41c350da3d9234491"
